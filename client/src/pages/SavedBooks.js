7beba02eeafcf9c245dbb3486780c1bc50eed5f6
KevinRhode <kevinrhode@hotmail.com>
2023-07-06 23:10:40 -0400

    add: quick build

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -6,7 +6,7 @@ import {
   Row,
   Col
 } from 'react-bootstrap';
-
+ 
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
